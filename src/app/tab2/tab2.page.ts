3005094a7b8da6b202dcba31fe49e15b21c7f0c5
dixit-99 <dixitsinh4501@gmail,com>
2020-02-27 19:12:01 +0530

    remove from wishlist

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit} from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { Platform, LoadingController } from '@ionic/angular';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { UserServiceService } from '../services/user-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page {
 
   books: any[];
   backButtonSubscription:any;
@@ -16,22 +17,33 @@ export class Tab2Page implements OnInit {
   constructor(
     private platform: Platform,
     private nativeStorage: NativeStorage,
-    private userService: UserServiceService
+    private userService: UserServiceService,
+    private router: Router,
+    private loadingController: LoadingController
   ) {}
 
-  ngOnInit() {
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: "crescent",
+      mode: "ios",
+      cssClass: "myspin"
+    });
+    return await loading.present();
+  }
+
+  ionViewWillEnter() {
+    this.presentLoadingWithOptions()
     this.nativeStorage.getItem('userId').then(
       (data) => { this.userService.getWishlist(data.user).subscribe(
                   (data) => {  this.books = data },
                   (error) => { console.log(error) },
-                  () => { }
+                  () => {
+                    this.loadingController.dismiss()
+                   }
                   )
                 },
       (error) => console.log(error)
     )
-  }
-
-  ionViewWillEnter() {
     this.backButtonSubscription = this.platform.backButton.subscribeWithPriority(4,() => {
       window.history.back();
     });
@@ -39,6 +51,19 @@ export class Tab2Page implements OnInit {
 
   ionViewDidLeave() {
     this.backButtonSubscription.unsubscribe();
+  }
+
+  rmWishlist(wishlistId) {
+    this.userService.rmWishlist(wishlistId).subscribe(
+      () => {},
+      (error) => { console.log(error) },
+      () => {this.ionViewWillEnter()}
+    )
+  }
+
+  bookDetails(bookid){
+    localStorage.setItem("bookid",bookid)
+    this.router.navigateByUrl("book-deatils");
   }
 
 }
